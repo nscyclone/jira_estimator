698f01944aa49937e8a7b2ac1d5c8989065f6ae8
nscyclone <nscyclone@gmail.com>
2026-06-08 16:13:20 +0300

    refactor: split risk and estimate prediction into two separate models

--- estimate_model.py
+++ estimate_model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 from transformers import AutoModel
 
-class MultiTaskRuBERT(nn.Module):
+class EstimateRuBERT(nn.Module):
     def __init__(self, model_name='cointegrated/rubert-tiny2', num_risk_classes=4):
         super().__init__()
         self.bert = AutoModel.from_pretrained(model_name)
@@ -11,9 +11,6 @@ class MultiTaskRuBERT(nn.Module):
         # Regression head - 1 number for FTE prediction
         self.regression_head = nn.Linear(hidden_size, 1)
 
-        # Classification head - `num_risk_classes` numbers for risk class prediction
-        self.classification_head = nn.Linear(hidden_size, num_risk_classes)
-
     def forward(self, input_ids, attention_mask):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
 
@@ -21,6 +18,5 @@ class MultiTaskRuBERT(nn.Module):
         cls_output = outputs.last_hidden_state[:, 0, :]
 
         logits_estimate = self.regression_head(cls_output).squeeze(-1)
-        logits_risk = self.classification_head(cls_output)
 
-        return logits_estimate, logits_risk
+        return logits_estimate
---
+++ risk_model.py
@@ -0,0 +1,22 @@
+import torch.nn as nn
+from transformers import AutoModel
+
+class RiskRuBERT(nn.Module):
+    def __init__(self, model_name='cointegrated/rubert-tiny2', num_risk_classes=4):
+        super().__init__()
+        self.bert = AutoModel.from_pretrained(model_name)
+
+        hidden_size = self.bert.config.hidden_size
+
+        # Classification head - `num_risk_classes` numbers for risk class prediction
+        self.classification_head = nn.Linear(hidden_size, num_risk_classes)
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+
+        # Get embedding of [CLS]-token
+        cls_output = outputs.last_hidden_state[:, 0, :]
+
+        logits_risk = self.classification_head(cls_output)
+
+        return logits_risk
